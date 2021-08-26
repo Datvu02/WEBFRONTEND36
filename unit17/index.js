2c503231834684d397dbc3051114c8bb7ae9ee76
Datvu02 <dvuquoc60@gmail.com>
2021-08-26 18:26:35 +0700

    unit17

---
+++ unit17/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	$('.jquery').on('click',function(){
+		 $('.none').removeClass('ative');
+		 $('.jquery').removeClass('ative_jquery');
+		 $(this).toggleClass('ative_jquery');
+		 $(this).next().toggleClass('ative');
+		 $(this).next().toggleClass('animate__fadeInDown');
+
+	});
+	$('#formDemo').validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 8
+			},
+			email: {
+				required: true,
+				email: true 	
+			},
+			number: {
+				required: true,
+				phone: true
+			}
+		},
+		messages: {
+			name: {
+				required: "Vui lòng nhập họ tên",
+				minlength: "Vui lòng nhập đầy đủ họ tên"
+			},
+			email: {
+				required: "Vui lòng nhập email",
+				email: "Email chưa đúng định dạng"
+			},
+			number: {
+				required: "Vui lòng nhập số điện thoại",
+				phone: "Số điện thoại chưa đúng định dạng"
+			}
+		}
+	})
+})
