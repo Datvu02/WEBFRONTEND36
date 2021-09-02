42297faa6955eac99106c80b5bd1319955e694d3
Datvu02 <dvuquoc60@gmail.com>
2021-09-02 17:17:55 +0700

    unit20
    df

--- unit20/index.js
+++ unit20/index.js
@@ -5,4 +5,14 @@ $(document).ready(function(){
 		$('.product_content1').eq($(this).index()).addClass('ative_product_content');
 		$(this).addClass('ative_btn');
 	})
+	$('.bars').on('click' ,function(){
+		$('.menu_min').slideDown(500);
+		$('.times').css('display' , 'block');
+		$('.bars').css('display' , 'none');
+	})
+	$('.times').on('click' ,function(){
+		$('.menu_min').slideUp(500);
+		$('.bars').css('display' , 'block');
+		$('.times').css('display' , 'none');
+	})
 })
