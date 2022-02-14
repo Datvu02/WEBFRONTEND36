237556cd71d2ba383829494e70be5fcadb66ec80
Datvu02 <dvuquoc60@gmail.com>
2022-02-14 12:41:32 +0700

    edit

--- unit20/index.js
+++ unit20/index.js
@@ -15,4 +15,24 @@ $(document).ready(function(){
 		$('.bars').css('display' , 'block');
 		$('.times').css('display' , 'none');
 	})
+  	var width = $(window).width();
+	if (width > 767) {
+		$('.bars').css('display' , 'none !important');
+	}
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:5
+	        }
+	    }
+	})
 })
